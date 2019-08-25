a82ccf3adb96f81ac619aa02a5713568db05d523
daniel mowbray <danielmowbray@mac.com>
2019-08-24 23:37:02 -0500

    initial

---
+++ PyBank/main.py
@@ -0,0 +1,53 @@
+import os 
+import csv
+
+#csv
+
+
+#Function Junction
+def bank(hw):
+
+#total months
+    month = 0   
+#total Profit/losses
+    total = 0
+#Average Change Profit/loss
+    avg_chng = 0
+#greatest increase (date/Amount)
+    max_rev = 0
+    max_month = ""
+#greatest decrease (date/amount)
+    min_rev = 0
+    min_month = ""
+    #loop
+    for row in csvfile:
+        current_month = row[0]
+        pal = row[1]
+        month += 1
+        total += row[1]
+        avg_chng = (total/month)
+        if pal > max_rev:
+            max_rev = pal
+            max_month = current_month
+        elif pal < min_rev:
+            min_rev = pal
+            min_month = current_month
+    return [months, total, avg_chng, max_month, max_rev, min_month, min_rev]
+
+#call
+#print
+#write
+
+with open(os.path.join('Resources', 'budget_data.csv')) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ',')
+    header = next(csvreader)
+    analysis = bank(csvreader)
+print(analysis[0, 1, 2, 3, 5, 4, 7, 6])
+
+write_path = 'users/'
+
+
+
+
+#print
+#write
---
+++ PyPoll/main.py
@@ -0,0 +1,24 @@
+import os
+import csv
+
+#function juction
+def count(file):
+    total_votes = 0
+    percent = 0
+    name = {}
+    winner = ''
+    
+    
+    for row in csvfile:
+        total_votes += 1
+        voteid = row[0]
+        vote = row[2]
+
+        if vote in name:
+    return[total_votes]
+
+with open(os.path.join('Resources', 'election_data.csv')) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ',')
+    header = next(csvreader)
+    results = count(csvreader)
+print()
